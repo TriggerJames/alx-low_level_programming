3c10e7f87a6c97eb49b1bc522dae09952ab68943
TriggerJames <Kimothojames254@gmail.com>
2023-11-15 05:58:39 -0800

    0x08. C
    Recursion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+return (result);
+}
