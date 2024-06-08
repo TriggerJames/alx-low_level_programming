3bb93cb00b7a698230c3cc026cedb8fd5b921cc2
TriggerJames <Kimothojames254@gmail.com>
2024-06-08 03:57:25 +0300

    0x1E. C
    Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,9 +10,9 @@
  */
 size_t minimum_value(size_t a, size_t b)
 {
-    if (b < a)
-        return (b);
-    return (a);
+	if (b < a)
+		return (b);
+	return (a);
 }
 
 /**
@@ -26,36 +26,37 @@ size_t minimum_value(size_t a, size_t b)
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t l, r, jump;
-    int val;
+	size_t l, r, jump;
+	int val;
 
-    if (array != NULL && size > 0)
-    {
-        jump = sqrt(size);
-        l = 0;
-        r = jump;
-        val = array[l];
-        printf("Value checked array[%lu] = [%d]\n", l, val);
-        while (r < size && val < value)
-        {
-            if (array[r] >= value)
-                break;
-            l += jump;
-            r += jump;
-            val = array[l];
-            printf("Value checked array[%lu] = [%d]\n", l, val);
-        }
-        if (l >= size || val > value)
-            return (-1);
-        printf("Value found between indexes [%lu] and [%lu]\n", l, r);
-        while (l <= minimum_value(size - 1, r) && val <= value)
-        {
-            val = array[l];
-            printf("Value checked array[%lu] = [%d]\n", l, val);
-            if (val == value)
-                return (l);
-            l++;
-        }
-    }
-    return (-1);
+	if (array != NULL && size > 0)
+	{
+		jump = sqrt(size);
+		l = 0;
+		r = jump;
+		val = array[l];
+		printf("Value checked array[%lu] = [%d]\n", l, val);
+		while (r < size && val < value)
+		{
+			if (array[r] >= value)
+				break;
+			l += jump;
+			r += jump;
+			val = array[l];
+			printf("Value checked array[%lu] = [%d]\n", l, val);
+		}
+		if (l >= size || val > value)
+			return (-1);
+		printf("Value found between indexes [%lu] and [%lu]\n", l, r);
+		while (l <= minimum_value(size - 1, r) && val <= value)
+		{
+			val = array[l];
+			printf("Value checked array[%lu] = [%d]\n", l, val);
+			if (val == value)
+				return (l);
+			l++;
+		}
+	}
+	return (-1);
 }
+
