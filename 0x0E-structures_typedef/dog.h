05394e599f3202a059185fe1e815f1f05e273a4e
TriggerJames <Kimothojames254@gmail.com>
2024-07-01 23:51:01 +0300

    0x0E. C
    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
