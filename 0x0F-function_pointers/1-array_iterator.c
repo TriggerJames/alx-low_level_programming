df27f7a19e6c2d6efc6c8043b9819040279af008
TriggerJames <Kimothojames254@gmail.com>
2024-07-02 01:40:12 +0300

    0x0F. C
    Function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - the iterator
+ * @array: the array
+ * @size: the size
+ * @action: one value
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+}
